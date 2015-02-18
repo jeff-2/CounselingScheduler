1ae717ce778bc802cc537d77e37394070114820b
Ryan <ramusa2@illinois.edu>
2015-02-18 15:30:48 -0600

    Added tester class, set up basic GUI elements in ClinicianIDListEditor

---
+++ src/gui/ClinicianIDListTester.java
@@ -0,0 +1,22 @@
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * Tester class to create a simple clinician ID list GUI frame.
+ * 
+ * @author ramusa2
+ * @author dtli2
+ */
+public class ClinicianIDListTester {
+
+	/**
+	 * Create an empty GUI frame 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClinicianIDListEditor editor = new ClinicianIDListEditor();
+	}
+
+}
