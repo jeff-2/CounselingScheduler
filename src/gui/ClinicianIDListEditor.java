8bb4dcc1d3d310eaab471bb8d9ef03b4eb72e092
Ryan <ramusa2@illinois.edu>
2015-02-18 15:02:38 -0600

    Added GUI file for client ID list

---
+++ src/gui/ClinicianIDListEditor.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * A GUI element for adding and removing clinicians from the clinician ID list. 
+ * Allows saving and loading clinician list to disk.
+ * 
+ * @author ramusa2
+ * @author dtli2
+ * 
+ */
+public class ClinicianIDListEditor extends JFrame {
+
+	/**
+	 * Generated Serial Version UID
+	 */
+	private static final long serialVersionUID = -5942052228994817185L;
+
+}
