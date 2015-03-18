6301b94c917d65ca15edf44511dde2078b883917
Jeffrey Foster <jmfoste2@illinois.edu>
2015-03-18 16:30:31 -0500

    Refactor NewSemesterSettings to use DateRangeValidator.

--- src/gui/admin/NewSemesterSettings.java
+++ src/gui/admin/NewSemesterSettings.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -22,6 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
+import validator.DateRangeValidator;
 import validator.InvalidDateRangeException;
 import bean.CalendarBean;
 import bean.HolidayBean;
@@ -280,7 +280,7 @@ public class NewSemesterSettings extends JFrame implements ActionListener {
 		}
 
 		try {
-			checkDateRange(semesterStart, semesterEnd);
+			DateRangeValidator.validate(semesterStart, semesterEnd);
 		} catch (InvalidDateRangeException e) {
 			JOptionPane.showMessageDialog(this,
 					"Cannot create new semester. " +
@@ -290,51 +290,6 @@ public class NewSemesterSettings extends JFrame implements ActionListener {
 			return false;
 		}
 		return true;
-	}
-
-	/**
-	 * Parses the date. Ensures it is in the proper date format MM/dd/yyyy with a year 
-	 * between 1800 and 10000. 
-	 *
-	 * @param date the date
-	 * @return the date
-	 * @throws ParseException the parse exception
-	 */
-	private Date parseDate(String date) throws ParseException {
-		Date d;
-		try {
-			SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-			format.setLenient(false);
-			d = format.parse(date);
-		} catch (ParseException e) {
-			throw new ParseException("The date " + date + " must be a valid date of the form mm/dd/yyyy. For example March 3, 1994 should be entered as 3/3/1994.", 0);
-		}
-		if (d.before(new SimpleDateFormat("MM/dd/yyyy").parse("01/01/1800"))) {
-			throw new ParseException("The year must be a year of the form yyyy, where yyyy is at least 1800", 0);
-		}
-		if (d.after(new SimpleDateFormat("MM/dd/yyyy").parse("12/31/9999"))) {
-			throw new ParseException("The year must be a year of the form yyyy, where yyyy is less than 10000", 0);
-		}
-
-		return d;
-	}
-
-	/**
-	 * Check for valid date range.
-	 *
-	 * @param startDate the start date
-	 * @param endDate the end date
-	 * @throws InvalidDateRangeException the invalid date range exception
-	 */
-	private void checkDateRange(String startDate, String endDate) throws InvalidDateRangeException {
-		try {
-			Date lower = parseDate(startDate);
-			Date upper = parseDate(endDate);
-			if (lower.after(upper))
-				throw new InvalidDateRangeException("The start date must be before the end date.");
-		} catch (ParseException e) {
-			throw new InvalidDateRangeException(e.getMessage());
-		}
 	}
 
 	/**
@@ -367,7 +322,7 @@ public class NewSemesterSettings extends JFrame implements ActionListener {
 		}
 
 		try {
-			checkDateRange(startDate, endDate);
+			DateRangeValidator.validate(startDate, endDate);
 		} catch (InvalidDateRangeException e) {
 			JOptionPane.showMessageDialog(this,
 					"Cannot add the given holiday to the list. " +
