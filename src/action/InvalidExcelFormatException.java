dd43c0135adabfc0ced78bf0b0fdab88c593d383
Jeffrey Foster <jmfoste2@illinois.edu>
2015-04-10 16:21:23 -0500

    jmfoste2, nbeltr2: Forgot to add InvalidExcelFormatException on last commit.

---
+++ src/action/InvalidExcelFormatException.java
@@ -0,0 +1,10 @@
+package action;
+
+public class InvalidExcelFormatException extends Exception {
+
+	private static final long serialVersionUID = -876532480101804183L;
+
+	public InvalidExcelFormatException(String message) {
+		super(message);
+	}
+}
