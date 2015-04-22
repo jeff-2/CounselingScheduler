1e1f7598ffffb43758146fdc0326149b1e378103
Kevin Lim <lim92@illinois.edu>
2015-04-22 15:19:25 -0500

    dtli2, lim92: Populated hashmaps that will be used for sorting and validation and updated Week class

--- src/bean/Schedule.java
+++ src/bean/Schedule.java
@@ -3,14 +3,17 @@ package bean;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import linearprogram.ScheduleProgram;
-
+import linearprogram.Week;
 import dao.CalendarDAO;
 import dao.ClinicianDAO;
 import dao.ClinicianPreferencesDAO;
@@ -24,12 +27,12 @@ public class Schedule {
 
 	private List<HashMap<SessionBean, Clinician>> ec;
 	private List<HashMap<SessionBean, List<Clinician>>> ia;
-	private HashMap<ClinicianBean, List<ClinicianWeekBean>> x;
+	private HashMap<Clinician, List<ClinicianWeekBean>> x;
 	
 	private CalendarBean calendar;
-	private List<Clinician> clinicians;
 	private List<HolidayBean> holidays;
-	private List<SessionBean> sessions;
+	private List<SessionBean> sessions;	
+	private HashMap<Integer, Clinician> clinicians;
 	
 	protected Connection conn;
 	
@@ -46,12 +49,14 @@ public class Schedule {
 		TimeAwayDAO timeAwayDAO = new TimeAwayDAO(conn);
 		CalendarDAO calendarDAO = new CalendarDAO(conn);
 		
-		List<ClinicianBean> clinicianBeans = clinicianDAO.loadClinicians();
+		ec = new ArrayList<HashMap<SessionBean, Clinician>>();
+		ia = new ArrayList<HashMap<SessionBean, List<Clinician>>>();
 		
+		List<ClinicianBean> clinicianBeans = clinicianDAO.loadClinicians();
 		
 		for(ClinicianBean cb : clinicianBeans) {
 			int clinicianID = cb.getClinicianID();
-			clinicians.add(new Clinician(cb,
+			clinicians.put(cb.getClinicianID(), new Clinician(cb,
 					clinicianPreferencesDAO.loadClinicianPreferences(clinicianID),
 					commitmentsDAO.loadCommitments(clinicianID),
 					timeAwayDAO.loadTimeAway(clinicianID)));
@@ -71,9 +76,49 @@ public class Schedule {
 	public Schedule loadScheduleFromDBAndAssignClinicians() throws SQLException {
 		Schedule schedule = loadScheduleFromDB();
 		ScheduleProgram.assignClinicians(schedule);
+		
+		List<Week> weeks = Week.getSemesterWeeks(calendar);
+		
+		// Initial fill of ia and ec
+		for (int i = 0; i < weeks.size(); i++) {
+			ec.add(new HashMap<SessionBean, Clinician>());
+			ia.add(new HashMap<SessionBean, List<Clinician>>());
+		}
+		
+		// Initial fill of x
+		for (Clinician c: clinicians.values()) {
+			List<ClinicianWeekBean> weekBeans = new ArrayList<ClinicianWeekBean>();
+			for (int i = 0; i < weeks.size(); i++) {
+				weekBeans.add(new ClinicianWeekBean());
+			}
+			x.put(c, weekBeans);
+		}
+
+		// The important part
+		for (SessionBean sb : sessions) {
+			List<Integer> clinicianIDs = sb.getClinicians();
+			int weekNum = weeks.indexOf(Week.getWeek(sb.getDate(), calendar));
+			// Handle ia/ec
+			if (sb.getType() == SessionType.IA) {
+				// This is an IA session.
+				List<Clinician> assigned = new ArrayList<Clinician>();
+				for (Integer id : clinicianIDs) {
+					assigned.add(clinicians.get(id));
+				}
+				ia.get(weekNum).put(sb, assigned);
+			} else {
+				// This is an EC session.
+				ec.get(weekNum).put(sb, clinicians.get(clinicianIDs.get(0)));
+			}
+			// Handle x
+			for (Integer id : clinicianIDs) {
+				x.get(clinicians.get(id)).get(weekNum).addSession(sb);
+			}
+		}
+		
 		return schedule;
 	}
-
+	
 	public List<HolidayBean> getHolidays() {
 		return holidays;
 	}
@@ -95,6 +140,10 @@ public class Schedule {
 	}
 
 	public List<Clinician> getClinicians() {
-		return clinicians;
+		Collection<Clinician> c = clinicians.values();
+		if (c instanceof List) {
+			return (List<Clinician>) c;
+		}
+		return new ArrayList<Clinician>(c);
 	}
 }
