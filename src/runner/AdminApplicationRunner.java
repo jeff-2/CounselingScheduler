e7ad4da8a9d655463ea21f6be79f370b63b634a0
Jeffrey Foster <jmfoste2@illinois.edu>
2015-05-03 10:10:06 -0500

    Forgot to commit AdminApplicationRunner which is used for uispec4j tests.

---
+++ src/runner/AdminApplicationRunner.java
@@ -0,0 +1,35 @@
+package runner;
+
+import generator.TestDataGenerator;
+import gui.admin.AdminApplication;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import javax.swing.UIManager;
+
+
+/**
+ * The Class AdminApplicationRunner runs the AdminApplication with the
+ * database populated with default test data.
+ */
+public class AdminApplicationRunner {
+    
+    /**
+     * Runs the admin application with test data.
+     *
+     * @param args
+     *            the arguments
+     * @throws ParseException, SQLException 
+     */
+    public static void main(String[] args) throws ParseException, SQLException {
+	try {
+	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} catch (Exception e) {
+	    System.out
+		    .println("Can't set system look and feel. Using default.");
+	}
+	TestDataGenerator.overwriteAndFillDemoData();
+	new AdminApplication();
+    }
+}
